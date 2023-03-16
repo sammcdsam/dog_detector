8ea9668713eeade0ca9a320100264e29f6c6e001
sammcdevitt <sammcdsam@gmail.com>
2023-03-16 12:06:15 -0500

    changing the command prefix

--- discord_bot.py
+++ discord_bot.py
@@ -15,7 +15,7 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.all()
 
-bot = commands.Bot(command_prefix="$", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 gif_shared = False
 update_time = datetime.datetime.now()
